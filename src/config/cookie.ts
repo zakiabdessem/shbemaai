d22426cd54587d2703ab1034d1aae02d08ebbe19
Zak <z.abdessmed@univ-batna2.dz>
2024-06-12 00:24:12 +0100

    final ver

--- src/config/cookie.ts
+++ src/config/cookie.ts
@@ -1,10 +1,10 @@
 function cookieConfig() {
-    return {
-      httpOnly: true,
-      sameSite: 'strict' as const,
-      secure: process.env.NODE_ENV === 'production',
-      domain: process.env.NODE_ENV === 'production' ? `${process.env.COOKIE_URL}` : 'localhost',
-    };
-  }
-  
-  export default cookieConfig;
+  return {
+    httpOnly: true,
+    sameSite: 'Strict',
+    secure: process.env.NODE_ENV === 'production',
+    domain: process.env.NODE_ENV === 'production' ? process.env.COOKIE_URL : 'localhost',
+  };
+}
+
+export default cookieConfig;
